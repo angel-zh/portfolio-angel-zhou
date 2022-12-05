2c3c49d99584d463e4876ed2c74a6708446d7bcf
Angel Zhou <angel.zhou1016@gmail.com>
2022-12-05 15:07:34 -0500

    sets up parallax

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
+import { ParallaxProvider, ParallaxBanner } from "react-scroll-parallax";
+import './index.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <ParallaxProvider>
+      <ParallaxBanner 
+        image='' 
+        speed={-20}
+        translateY={[]} />
+
+
+      
+    </ParallaxProvider>
+  )
 }
 
-export default App;
+export default App
