8f015d4a8c2a2c896bccd0a5300f10a26e982f37
Angel Zhou <angel.zhou1016@gmail.com>
2022-12-23 10:21:42 -0500

    removes scrolltotop icon

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,4 @@
 import React from 'react'
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTurnUp } from '@fortawesome/free-solid-svg-icons'
 import layer3 from '../images/3.png'
 import layer4 from '../images/4.png'
 
@@ -9,9 +7,6 @@ const Footer = () => {
         <div className='bottom-div'>
             <img src={layer4} className='bottom-layer' alt=''></img>
             <img src={layer3} className='bottom-layer' alt=''></img>
-            <span onClick={() => { window.scrollTo({ top: 0, left: 0, behavior: 'smooth' }) }}>
-                <FontAwesomeIcon icon={faTurnUp} className='nav-icon to-top-icon' />
-            </span>
             <footer>
                 &copy; 2022 Angel Zhou <br />
                 All visual elements, aside from icons, were self-produced.
