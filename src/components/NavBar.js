303583de4c10897b27c310bf7ea712923478a965
Angel Zhou <angel.zhou1016@gmail.com>
2023-02-12 13:54:44 -0500

    updates resume link

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -13,7 +13,7 @@ const NavBar = ({ handleScrollOnClick }) => {
       <a href='https://www.linkedin.com/in/angel-q-zhou/' target='_blank' rel='noopener noreferrer'>
         <FontAwesomeIcon icon={faLinkedin} className='nav-icon' />
       </a>
-      <a href='https://docs.google.com/document/d/1z5I7XAXyyFZh09DxDBj6HcFSKTCtfEoqTI3iEUT6ID8/edit?usp=sharing' target='_blank' rel='noopener noreferrer'>
+      <a href='https://docs.google.com/document/d/1BgJrRqY7EziTPKyjEBWZFsmFEyREiFeB/edit?usp=sharing&ouid=113510718493931215820&rtpof=true&sd=true' target='_blank' rel='noopener noreferrer'>
         <FontAwesomeIcon icon={faFileLines} className='nav-icon file-icon' />
       </a>
       <span onClick={handleScrollOnClick}>
